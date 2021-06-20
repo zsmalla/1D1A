09e09b1bd779c7a1e0c404e1ab9b698766167b7b
zsmalla <malla7454@naver.com>
2021-06-20 16:31:08 +0900

    third commit

---
+++ LEVEL1/java/폰켓몬/Solution.java
@@ -0,0 +1,20 @@
+package LEVEL1.java.폰켓몬;
+
+import java.util.HashSet;           // java set interface
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer;
+        int n = nums.length;        // array size check
+        int N = n/2;                // 가져올 수 있는 폰켓몬 수
+        HashSet<Integer> a = new HashSet<Integer>();
+        for (int i = 0; i < n; i++){
+            a.add(nums[i]);
+        }
+
+        if (a.size() < N) answer = a.size();
+        else answer = N;
+        
+        return answer;
+    }
+}
